28651c6711c18640423cbb3b6ea73a1ae28f6579
Vadim <angryvadik@gmail.com>
2023-06-04 02:59:54 +0200

    feat: sort

---
+++ src/components/Sort.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+type SortProps = {
+  onSort: (dir: "asc" | "desc" | "none") => void;
+};
+
+const Sort = ({ onSort }: SortProps) => {
+  const dirs = ["asc", "desc", "none"] as const;
+  const [curIdx, setCurIdx] = useState(0);
+  return (
+    <button
+      onClick={() => {
+        setCurIdx((curIdx + 1) % 3);
+        onSort(dirs[(curIdx + 1) % 3]);
+      }}
+    >
+      {dirs[curIdx]}
+    </button>
+  );
+};
+
+export default Sort;
